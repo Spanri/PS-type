a7332da94346a368e96ef3f24d2074ed8252f133
Anna Kozlova <kozlova9v@mail.ru>
2017-04-13 19:26:01 +0300

    return json in error

--- app.js
+++ app.js
@@ -34,9 +34,10 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send({
+    status: 'error',
+    message: err.message || 'Server error'
+  });
 });
 
 module.exports = app;
