2a1041ad3edf037bddbdd7690e489bb447f45c25
Anna Kozlova <kozlova9v@mail.ru>
2017-07-11 11:59:41 +0200

    map

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,10 +2,12 @@
 import express from 'express';
 import auth from './auth';
 import change from './change';
+import map from './map';
 const router = express.Router();
 
 router.use('/', auth);
 router.use('/change', change);
+router.use('/map', map);
 
 router.get('/', (req, res, next) => {
   res.json({
