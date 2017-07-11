f3477acd2894d0ad14837d8590789208aa83ea11
Anna Kozlova <kozlova9v@mail.ru>
2017-07-11 23:31:13 +0200

    change

--- routes/api/v1/change.js
+++ routes/api/v1/change.js
@@ -4,70 +4,56 @@ import express from 'express';
 import User, {validator} from '../../../models/user';
 import Uservk from '../../../models/uservk';
 import { dberr, catchErr } from '../../../helpers';
+import jwt_decode from 'jwt-decode';
 const router = express.Router();
 
-router.post('/vk', async (req, res, next) => {
-let uservk = null;
-  try {
-    uservk = await Uservk.findOne({ idvk: req.body.idvk }).exec();
-    if (!uservk) return res.status(404).send({
-      status: 'error',
-      message: 'User not found'
-    });
-  } catch (err) { return dberr(res); }
-try{
-    if (req.body.age) uservk.age = req.body.age;
-    if (req.body.sex) uservk.sex = req.body.sex;
-    uservk.save(function(err) {
-       if (err) {if (err.name === 'ValidationError') {
-      const firstErr = err.errors[Object.keys(err.errors)[0]];
-      let message = 'Unexpected error';
-      if (firstErr.message) message = firstErr.message;
-      return res.status(400).send({
-        status: 'error',
-        message
-      });
-       } }
-    else return res.status(200).send({
-        status: 'ok',
-        message: 'User successfuly changed'
-    });
-    });
-} catch(err) { return dberr(res); }
-});
+//токены для запросов в постмане, пользователи asdfgh
+//vk "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZHZrIjoiYXNkZmdoIiwiX2lkIjoiNTk2NTNjYzM2NzA5MzExOTQ0YzRhZjlmIiwiaWF0IjoxNDk5ODA2OTE2LCJleHAiOjE1MDA2NzA5MTZ9.a1tV8GwWQhtzZ89F3kkNYLlRoE10VbOx8MkMxjTGDFU"
+//"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFzZGZnaCIsIl9pZCI6IjU5NjUzZWE1NjcwOTMxMTk0NGM0YWZhMCIsImlhdCI6MTQ5OTgwNzM5NywiZXhwIjoxNTAwNjcxMzk3fQ.Ww4xxFd65Oez0hVPED4mH4NaiVn9IeD8Hl0DckjEdJY"
 
 router.post('/', async (req, res, next) => {
-    let user = null;
+  let token = jwt_decode(req.body.token);
+  let user = null, uservk = null;
   try {
-    user = await User.findOne({ username: req.body.username }).exec();
-    if (!user) return res.status(404).send({
+    user = await User.findOne({ _id: token._id }).exec();
+    uservk = await Uservk.findOne({ _id: token._id }).exec();
+    if (!user && !uservk) return res.status(404).send({
       status: 'error',
       message: 'User not found'
     });
   } catch (err) { return dberr(res); }
-try{
-  //Простите, я не могу поймать эти ошибки catch-ем, не знаю почему
-   if (req.body.age) if (req.body.age<14) 
-     return res.status(400).send({
-         status: 'error',
-         message: 'Age must be >=14'
-     }); else if (req.body.age>110) 
-     return res.status(400).send({
-         status: 'error',
-         message: 'Age must be <=110'
-     }); else
-     User.update( { username: req.body.username } , { $set: { age: req.body.age} }, function(err){});
-  if (req.body.sex) if(!(typeof(req.body.sex) === "boolean")) 
+
+  if (req.body.age) if (req.body.age < 14)
     return res.status(400).send({
-        status: 'error',
-        message: 'Sex must be boolean (true for male)'
-    }); else
-    User.update( { username: req.body.username } , { $set: {sex: req.body.sex } }, function(err){});
-    return res.status(200).send({
-        status: 'ok',
-        message: 'User successfuly changed'
+      status: 'error',
+      message: 'Age must be >=14'
+    }); else if (req.body.age > 110)
+    return res.status(400).send({
+      status: 'error',
+      message: 'Age must be <=110'
     });
-} catch(err) {return dberr(res);}
+  if (req.body.sex) if (!(typeof (req.body.sex) === "boolean"))
+    return res.status(400).send({
+      status: 'error',
+      message: 'Sex must be boolean (true for male)'
+    });
+
+  if (user) try {
+    if (req.body.age) User.update(user.username, { $set: { age: req.body.age } }, function (err) { });
+    if (req.body.sex) User.update(user.username, { $set: { sex: req.body.sex } }, function (err) { });
+    return res.status(200).send({
+      status: 'ok',
+      message: 'User successfuly changed'
+    });
+  } catch (err) { return dberr(res); }
+  else try {
+    if (req.body.age) Uservk.update(uservk.idvk, { $set: { age: req.body.age } }, function (err) { });
+    if (req.body.sex) Uservk.update(uservk.idvk, { $set: { sex: req.body.sex } }, function (err) { });
+    return res.status(200).send({
+      status: 'ok',
+      message: 'User successfuly changed'
+    });
+  } catch (err) { return dberr(res); }
 });
 
 export default router;
