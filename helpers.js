b3891c34fc1c8b44de96ac66da7d8ccaf6cc8403
Anna Kozlova <kozlova9v@mail.ru>
2017-07-13 19:42:21 +0200

    other

--- helpers.js
+++ helpers.js
@@ -6,3 +6,17 @@ export function dberr(res) {
         message: 'Database error'
     });
 }
+
+export function ok(res) {
+return res.status(200).send({
+      status: 'ok',
+      message: 'User successfuly changed'
+}); 
+}
+
+export function notFound(res) {
+return res.status(404).send({
+      status: 'error',
+      message: 'User not found'
+    });
+}
