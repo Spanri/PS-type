b55f235e0b36207fd2042627cd0a53e8c45d35c0
Anna Kozlova <kozlova9v@mail.ru>
2017-12-12 19:25:38 +0200

    gui

--- src/gui/track.js
+++ src/gui/track.js
@@ -1,7 +1,5 @@
 "use strict"
 
-console.log('sdfg');
-
 $(document).ready(function () {
     var token, all;
     $("#login").click(()=>{
@@ -14,7 +12,8 @@ $(document).ready(function () {
                 (data) => {
                     if (data.username == 'admin0' || data.username == 'id136955296') {
                         $("#auth").css('display','none');
-                        $("#hello").html('Admin`s panel');
+                        $("#hello").css('display','block');
+                        $("#line").css('height','100%');
                         $("#panel").css('display','block');
                         $.post('/api/v1/data/all',
                             { "token": token },
@@ -23,49 +22,84 @@ $(document).ready(function () {
                                 console.log(dataAll.all);
                                 for (let k = 0; k < all.length; k++)
                                     $("#data").append(`<p>${all[k].username}</p>`);
+                                $("#data p").addClass('user');
                             }).fail(function (response, status, error) {
                                 console.log('Error: ' + response.responseText);
+                                $("#error").html(error);
+                                $("#error").css('display','block');
+                                setInterval(()=>{
+                                    $("#error").css('display','none');
+                                },3000);
                             });
                     }
-                    else $("#tr").html('Ok, but you are not admin:^(');
+                    // else $("#tr").html('Ok, but you are not admin:^(');
+                    else {
+                        $("#error").html('Ok, but you are not admin:^(');
+                        $("#error").css('display','block');
+                        setInterval(()=>{
+                            $("#error").css('display','none');
+                        },2000);
+                    }
                 }).fail(function (response, status, error) {
                     console.log('Error: ' + response.responseText);
-                    $("#tr").html(error);
+                    // $("#tr").html(error);
+                    $("#error").html(error);
+                    $("#error").css('display','block');
+                    setInterval(()=>{
+                        $("#error").css('display','none');
+                    },3000);
                 });
         }).fail(function (response, status, error) {
             console.log('Error: ' + response.responseText);
-            $("#tr").html(error);
+            // $("#tr").html(error);
+            $("#error").html(error);
+            $("#error").css('display','block');
+            setInterval(()=>{
+                $("#error").css('display','none');
+            },2000);
         });
     });
-    $("#watch").click(()=>{
+    var search = function(name){
         var i = -1;
-        let name = $('#username2').val();
         for (let k = 0; k < all.length; k++)
-            if (name == all[k].username) {
-                i = k;
-                var all2 = {
-                    'username': all[i].username,
-                    'age': all[i].age,
-                    'date': all[i].date,
-                    'latitude': all[i].latitude,
-                    'longitude': all[i].longitude,
-                    'type': all[i].obr.type,
-                    'date': all[i].obr.date,
-                    'sex': all[i].sex,
-                    'speed': all[i].speed
-                };
-                all2.latitude = all2.latitude.toString().replace(/,/g,' ');
-                all2.longitude = all2.longitude.toString().replace(/,/g,' ');
-                all2.speed = all2.speed.toString().replace(/,/g,' ');
-                $("#data p").remove();
-                for (var el in all2) $("#data").append(`<p>${el}: ${all2[el]}</p>`);
-            }
-        if(i==-1) $("#data").append(`<p>User not found</p>`);
+        if (name == all[k].username) {
+            i = k;
+            var all2 = {
+                'username': all[i].username,
+                'age': all[i].age,
+                'date': all[i].date,
+                'latitude': all[i].latitude,
+                'longitude': all[i].longitude,
+                'type': all[i].obr.type,
+                'date': all[i].obr.date,
+                'sex': all[i].sex,
+                'speed': all[i].speed
+            };
+            all2.latitude = all2.latitude.toString().replace(/,/g,' ');
+            all2.longitude = all2.longitude.toString().replace(/,/g,' ');
+            all2.speed = all2.speed.toString().replace(/,/g,' ');
+            $("#data p").remove();
+            for (var el in all2) $("#data").append(`<p>${el}: ${all2[el]}</p>`);
             
+        }
+        if (i == -1) { 
+            $("#error").html('User is not found');
+            $("#error").css('display','block');
+            setInterval(()=>{
+                $("#error").css('display','none');
+            },2000);
+        }
+    }
+    $("#watch").click(()=>{
+        search($('#username2').val());  
     });
-
-    
-//     var all = await User.find({}).exec();
-//     for(i in all)
-//         $("#tr").append(`<p>${all[i]}</p>`);
+    $("#back").click(()=>{
+        $("#data p").remove();
+        for (let k = 0; k < all.length; k++)
+            $("#data").append(`<p>${all[k].username}</p>`);
+        $("#data p").addClass('user');
+    });
+    $(document).on("click",".user",function(){
+        search(this.innerHTML);
+    });
 });
