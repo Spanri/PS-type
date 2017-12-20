ce5c54f968a60c6d77b2a2add0295e9bd66d3754
Anna Kozlova <kozlova9v@mail.ru>
2017-12-20 21:18:08 +0200

    map gui

--- src/gui/track.js
+++ src/gui/track.js
@@ -1,25 +1,36 @@
 "use strict"
 
 $(document).ready(function () {
+    $("#login").css("width",`${$("#password").width()+16}`);
+    $('#username, #password').focus(function(){
+        $(this).attr('placeholder','');
+    });
+    $('#username').focusout(function(){
+        $(this).attr('placeholder','Логин');
+    });
+    $('#password').focusout(function(){
+        $(this).attr('placeholder','Пароль');
+    });
+
     var token, all;
     $("#login").click(()=>{
-        console.log('click');
-        let username = $('#username').val();
-        let password = $('#password').val();
-        $.post('/api/v1/signin', { "username": username, "password": password }, (auth) => {
-            console.log('sigin');
+        let username = $("#username").val();
+        let password = $("#password").val();
+        $.post("/api/v1/signin", { "username": username, "password": password }, (auth) => {
+            console.log("Авторизация успешна");
             token = auth.token;
-            $.post('/api/v1/data',
+            $.post("/api/v1/data",
                 { "token": token },
                 (data) => {
-                    if (data.username == 'admin0' || data.username == 'id136955296') {
-                        console.log('admin0');
-                        $("#auth").css('display','none');
-                        $("#interface").css('display','block');
-                        $("#hello").css('display','block');
-                        $("#line").css('height','auto');
-                        $("#panel").css('display','block');
-                        $.post('/api/v1/data/all',
+                    if (data.username == "admin0" || data.username == "id136955296") {
+                        console.log("Загрузка данных");
+                        $("#auth").css("display","none");
+                        $("#base").css("display","grid").css("background-image","none");
+                        $("#interface").css("display","block");
+                        $("#hello").css("display","block");
+                        $("#line").css("height","auto");
+                        $("#panel").css("display","block");
+                        $.post("/api/v1/data/all",
                             { "token": token },
                             (dataAll) => {
                                 all = dataAll.all;
@@ -27,40 +38,37 @@ $(document).ready(function () {
                                 $("table").append(`<tr><th>Username</th><th>Age</th><th>Sex</th><th>Статистика</th></tr>`);
                                 for (let k = 0; k < all.length; k++)
                                     $("table").append(`<tr><td>${all[k].username}</td><td>${all[k].age}</td><td>${all[k].sex}</td><td>${all[k].obr.type}</td></tr>`);
-                                $("table tr").not('table th').addClass('user');
+                                $("table tr").not("table th").addClass("user");
                             }).fail(function (response, status, error) {
-                                console.log('Error: ' + response.responseText);
+                                console.log("Error: " + response.responseText);
                                 $("#error").html(error);
-                                $("#error").css('display','block');
+                                $("#error").css("display","block");
                                 setInterval(()=>{
-                                    $("#error").css('display','none');
+                                    $("#error").css("display","none");
                                 },3000);
                             });
                     }
-                    // else $("#tr").html('Ok, but you are not admin:^(');
                     else {
-                        $("#error").html('Круто, но ты не админ:^(');
-                        $("#error").css('display','block');
+                        $("#error").html("Круто, но ты не админ:^(");
+                        $("#error").css("display","block");
                         setInterval(()=>{
-                            $("#error").css('display','none');
+                            $("#error").css("display","none");
                         },2000);
                     }
                 }).fail(function (response, status, error) {
-                    console.log('Error: ' + response.responseText);
-                    // $("#tr").html(error);
+                    console.log("Error: " + response.responseText);
                     $("#error").html(error);
-                    $("#error").css('display','block');
+                    $("#error").css("display","block");
                     setInterval(()=>{
-                        $("#error").css('display','none');
+                        $("#error").css("display","none");
                     },3000);
                 });
         }).fail(function (response, status, error) {
-            console.log('Error: ' + response.responseText);
-            // $("#tr").html(error);
+            console.log("Error: " + response.responseText);
             $("#error").html(error);
-            $("#error").css('display','block');
+            $("#error").css("display","block");
             setInterval(()=>{
-                $("#error").css('display','none');
+                $("#error").css("display","none");
             },2000);
         });
     });
@@ -70,82 +78,103 @@ $(document).ready(function () {
         if (name == all[k].username) {
             i = k;
             var all2 = {
-                'username': all[i].username,
-                'age': all[i].age,
-                'date': all[i].date,
-                'latitude': all[i].latitude,
-                'longitude': all[i].longitude,
-                'type': all[i].obr.type,
-                'date': all[i].obr.date,
-                'sex': all[i].sex,
-                'speed': all[i].speed
+                "username": all[i].username,
+                "age": all[i].age,
+                "date": all[i].date,
+                "latitude": all[i].latitude,
+                "longitude": all[i].longitude,
+                "type": all[i].obr.type,
+                "date": all[i].obr.date,
+                "sex": all[i].sex,
+                "speed": all[i].speed
             };
-            all2.latitude = all2.latitude.toString().replace(/,/g,' ');
-            all2.longitude = all2.longitude.toString().replace(/,/g,' ');
-            all2.speed = all2.speed.toString().replace(/,/g,' ');
+            all2.latitude = all2.latitude.toString().replace(/,/g," ");
+            all2.longitude = all2.longitude.toString().replace(/,/g," ");
+            all2.speed = all2.speed.toString().replace(/,/g," ");
             $("tr,#data p").remove();
             for (var el in all2) $("#data").append(`<p>${el}: ${all2[el]}</p>`);
-            $("#base").css('height','auto')
+            $("#base").css("height","auto");
             
         }
-        if (i == -1) { 
-            $("#error").html('Пользователь не найден');
-            $("#error").css('display','block');
+        if(i == -1){
+            $("#error").html("Пользователь не найден");
+            $("#error").css("display","block");
             setInterval(()=>{
-                $("#error").css('display','none');
+                $("#error").css("display","none");
             },2000);
         }
     }
 
     $("#search").click(()=>{
-        $("#map").css('display','none');
-        // let user = $()
-        search($('#username2').val());  
+        $("#map").css("display","none");
+        search($("#username2").val());
     });
 
     //карты
     var map;
+    function initMap() {
+        var myLatlng = new google.maps.LatLng(55.751244, 37.618423);
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatlng,
+          zoom: 8
+        });
+    }
+
     $("#mapbut").click(()=>{
-        $("ymaps").remove();
-        ymaps.ready(init);
-    
-        function init(){     
-            map = new ymaps.Map("map", {
-                center: [55.76, 37.64],
-                zoom: 7
-            });
-        }
-    
         $("tr, #data p").remove();
-        $("#map").css('display','block');
+        initMap();
+        $("#map").css("display","block");
         for (let k = 0; k < all.length; k++)
             $("#data").append(`<p>${all[k].username}</p>`);
-        $("#data p").addClass('mapUser');
+        $("#data p").addClass("mapUser");
     });
 
-    $('#hide').click(()=>{
-        let panel = $('#interface').children();
-        if($('#hello').css('display') == 'block'){
-            $('#line').css('min-width','30px !important');
-            for(let i=0;i<panel.length;i++) panel[i].style.display = 'none';
-            $('#hide').css('display','block');
-            $('#hide').html('Открыть');
+    var flightPath;
+    $(document).on("click",".mapUser",async function(){
+        if(flightPath) flightPath.setMap(null);
+        let name = this.innerHTML;
+        let geometry = Array();
+
+        for (let k = 0; k < all.length; k++)
+            if (name == all[k].username) {
+                for(let i = 0;i<all[k].longitude.length;i++){
+                    await geometry.push({'lat':all[k].latitude[i],'lng':all[k].longitude[i]});
+                }
+                break;
+            }
+            flightPath = new google.maps.Polyline({
+                path: geometry,
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
+            flightPath.setMap(map);
+    });
+
+    $("#hide").click(()=>{
+        let panel = $("#interface").children();
+        if($("#hello").css("display") == "block"){
+            $("#line").css("min-width","30px !important");
+            for(let i=0;i<panel.length;i++) panel[i].style.display = "none";
+            $("#hide").css("display","block");
+            $("#hide").html("Открыть");
         }
         else{
-            for(let i=0;i<panel.length;i++) panel[i].style.display = 'block';
-            $('#hide').css('display','block');
-            $('#hide').html('Закрыть');
+            for(let i=0;i<panel.length;i++) panel[i].style.display = "block";
+            $("#hide").css("display","block");
+            $("#hide").html("Закрыть");
         }
     });
 
     //назад
     $("#back").click(()=>{
-        $("#map").css('display','none');
+        $("#map").css("display","none");
         $("tr, #data p").remove();
         $("table").append(`<tr><th>Username</th><th>Age</th><th>Sex</th><th>Статистика</th></tr>`);
         for (let k = 0; k < all.length; k++)
             $("table").append(`<tr><td>${all[k].username}</td><td>${all[k].age}</td><td>${all[k].sex}</td><td>${all[k].obr.type}</td></tr>`);
-        $("table tr").addClass('user');
+        $("table tr").addClass("user");
     });
 
     $(document).on("click",".user",function(){
@@ -153,45 +182,12 @@ $(document).ready(function () {
         search(user.innerHTML);
     });
 
-    $(document).on("click",".mapUser",async function(){
-        let name = this.innerHTML;
-        let geometry = Array();
-
-        for (let k = 0; k < all.length; k++)
-            if (name == all[k].username) {
-                for(let i = 0;i<all[k].longitude.length;i++){
-                    await geometry.push([all[k].longitude[i],all[k].latitude[i]]);
-                }
-                break;
-            }
-
-        let properties = {balloonContent: "Ломаная линия"};
-        let options = {
-            balloonCloseButton: false,
-            // Цвет линии.
-            strokeColor: "#000000",
-            // Ширина линии.
-            strokeWidth: 4,
-            // Коэффициент прозрачности.
-            strokeOpacity: 0.5
-        };
-        var polyline = new ymaps.Polyline(
-            geometry,
-            properties,
-            options
-        );
-        map.geoObjects.add(polyline);
-        // map.container.fitToViewport();
-
-        // polyline.editor.startEditing();
+    $("#username2").focus(()=>{
+        $("#search2").css("fill","white");
     });
 
-    $('#username2').focus(()=>{
-        $('#search2').css('fill','white');
-    });
-
-    $('#username2').blur(()=>{
-        $('#search2').css('fill','#827717');
-        $('#search2').css('transition','fill 0.3s ease-out');
+    $("#username2").blur(()=>{
+        $("#search2").css("fill","#827717");
+        $("#search2").css("transition","fill 0.3s ease-out");
     });
 });
