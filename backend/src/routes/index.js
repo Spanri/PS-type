3ecc0839c60fae760106282b22a32d38ba5d937b
Spanri <kozlova9v@mail.ru>
2019-09-15 10:36:50 +0300

    npm run build

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -20,23 +20,5 @@ router.use('/api', api);
 
 var serveStatic = require('serve-static')
 router.use('/', serveStatic(__dirname + "../../../dist"));
-router.use('/assets', serveStatic(__dirname + "../../../dist/css"));
-
-// router.use('/static', express.static(__dirname + '/dist'))
-
-// router.get('/m', (req, res) => {
-//   res.sendFile(path.join(__dirname + '../../../dist/index.html'));
-// });
-
-// router.use('/static', expressOrig.static(path.join(__dirname + '../../../dist/static')))
-
-// router.get('/', (req, res, next) => {
-//   res.json({
-//     name: "PSType API",
-//     madeBy: "Anna, Valya",
-//     versionOfServer: "0.1.1",
-//     versionOfClient: "0.0.1"
-//   });
-// });
 
 export default router;
